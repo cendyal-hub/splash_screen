bf0a0f406c4262cea223b4eb91f949d012e17980
cendy <sendyaja17@gmail.com>
2025-10-31 09:31:41 +0700

    menambahkan 3 bullet kecil

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -8,61 +8,78 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-        //Todo:
-        // karena akan membuat multiple widget
-        // maka membuat children yang bisa menampung banyak
-        children: [
-          SizedBox(height: 50),
-          Container(
-            width: 240,
-            height: 240,
-            // membuat Image bulat
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.cyan,
-              image: DecorationImage(
-                image: AssetImage("assets/images/image1.png"),
-                fit: BoxFit.cover,
-              ),
-              ),
-          ),
-          SizedBox(height: 20),
-          Text("Welcome",
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Text("forgot to bring your wallet\n You can ",
-            style: TextStyle(
-              fontSize: 12.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.green,
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 10,
-                width: 10,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.amberAccent,
+          //Todo:
+          // karena akan membuat multiple widget
+          // maka membuat children yang bisa menampung banyak
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 240,
+              height: 240,
+              // membuat Image bulat
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.cyan,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/image1.png"),
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
-          )
-        // widget "children"
-        // Membuat Title dengan warna hitam
-        // Membuat subtitle
-        // Membuat 3 bullet kecil
-        //. mulai dari warna terang diawal
-        // Membuat Button Continue
-        ],
-      ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "forgot to bring your wallet\n When you are shopping ? ",
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            // Membuat 3 bullet kecil
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: const Color.fromARGB(255, 69, 173, 73),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: const Color.fromARGB(255, 69, 173, 73),
+                  ),
+                ),
+              ],
+            ),
+            // widget "children"
+            // Membuat Title dengan warna hitam
+            // Membuat subtitle
+            //. mulai dari warna terang diawal
+            // Membuat Button Continue
+          ],
+        ),
       ),
     );
   }
