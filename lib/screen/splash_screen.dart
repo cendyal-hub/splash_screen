b64dcecb51b468f3f99e93c46fea4391b7727b03
cendy <sendyaja17@gmail.com>
2025-10-31 09:25:20 +0700

    menambahkan teks welcome dan lainnya

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -14,27 +14,54 @@ class SplashScreen extends StatelessWidget {
         children: [
           SizedBox(height: 50),
           Container(
-            width: 250,
-            height: 250,
+            width: 240,
+            height: 240,
+            // membuat Image bulat
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.cyan,
               image: DecorationImage(
                 image: AssetImage("assets/images/image1.png"),
                 fit: BoxFit.cover,
-              )
               ),
+              ),
+          ),
+          SizedBox(height: 20),
+          Text("Welcome",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text("forgot to bring your wallet\n You can ",
+            style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 10,
+                width: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.amberAccent,
+                ),
+              )
+            ],
           )
         // widget "children"
-        // membuat Image bulat
-
         // Membuat Title dengan warna hitam
         // Membuat subtitle
         // Membuat 3 bullet kecil
         //. mulai dari warna terang diawal
         // Membuat Button Continue
         ],
-        
       ),
       ),
     );
